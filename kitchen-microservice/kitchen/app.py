499e924198b6fa1c8417dc3288b47049a7ef65ec
mostafa-elnahal <mostafa.salim.egy@gmail.com>
2025-09-05 02:18:05 +0300

    overwrite flask automatic documentation

--- kitchen-microservice/kitchen/app.py
+++ kitchen-microservice/kitchen/app.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+import yaml
+from apispec import APISpec
+
 from flask import Flask
 from flask_smorest import Api
 
@@ -8,7 +12,15 @@ from api.api import blueprint
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 
+api_spec = yaml.safe_load((Path(__file__).parent / "oas.yaml").read_text())
+spec = APISpec(
+    title=api_spec["info"]["title"],
+    version=api_spec["info"]["version"],
+    openapi_version=api_spec["openapi"],
+)
+spec.to_dict = lambda: api_spec
+
+app.config["API_SPEC"] = spec
 kitchen_api = Api(app)
 
 kitchen_api.register_blueprint(blueprint)
-
